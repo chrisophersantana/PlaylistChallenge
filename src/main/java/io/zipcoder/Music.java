035655ef5909a1da96a9c477e51c6b5a1d0977d9
Christopher Santana <chris@zipcoders-MBP-6.fios-router.home>
2019-11-17 14:02:43 -0500

    finished lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,42 @@
 package io.zipcoder;
 
+import java.util.Map;
+
 public class Music {
 
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int clickNext = 0;
+        int clickBack = 0;
+
+        for(int i = startIndex; i <= playList.length;i++){
+
+            if(playList[i].equals(selection)){
+                break;
+            }
+            clickNext++;
+
+            if(i == (playList.length -1)) {
+                i = -1;
+            }
+        }
+
+        for(int j = startIndex; j >= 0 ; j --){
+
+            if(playList[j].equals(selection)){
+                break;
+            }
+            clickBack++;
+            if(j == 0){
+                j = playList.length;
+            }
+        }
+        return Math.min(clickNext, clickBack);
     }
 }
